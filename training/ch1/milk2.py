fdd3a98fc18d0307a946b90975c79ac012a00eb6
liulyan <yan.b.liu@oracle.com>
2024-10-13 14:57:04 -0400

    Add milk2 solution

---
+++ training/ch1/milk2.py
@@ -0,0 +1,48 @@
+"""
+ID: gallenl1
+LANG: PYTHON3
+TASK: milk2
+"""
+
+milkingTimes = []
+
+# read input
+with open("milk2.in", "r") as fIn:
+    N = int(fIn.readline().strip())
+    for _ in range(N):
+        line = fIn.readline().strip() # "300 1000" 
+        timeRange = [int(word) for word in line.split()] # "300 1000" -> ["300", "1000"] -> [300, 1000]
+        milkingTimes.append(timeRange)
+        
+
+# sort by start
+milkingTimes.sort()
+
+# combined time range
+combinedTimes = []
+
+maxGap = 0
+
+curS, curE = milkingTimes[0]
+
+for i in range(1, N):
+    s, e = milkingTimes[i]
+    if s <= curE: # overlapping
+        curE = max(curE, e)
+    else: # not overlapping
+        combinedTimes.append([curS, curE]) # finalize a merging range
+        maxGap = max(maxGap, s - curE)
+        # start a new merging range
+        curS = s
+        curE = e
+        
+combinedTimes.append([curS, curE])
+        
+maxMilkingTime = 0
+for s, e in combinedTimes:
+    maxMilkingTime = max(maxMilkingTime, e - s)
+    
+
+# write output
+with open("milk2.out", "w") as fOut:
+    fOut.write(f"{maxMilkingTime} {maxGap}\n")
