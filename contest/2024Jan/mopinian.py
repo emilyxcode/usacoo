6bce305cef419d43e9f9e589abcbfd98a9f350e9
BLUE\emily <emily@emily>
2025-01-05 12:14:08 -0500

    Add majority opinian problem

---
+++ contest/2024Jan/mopinian.py
@@ -0,0 +1,32 @@
+#import sys
+#sys.stdin = open("mopinian.in")
+
+def solve(n, cows):
+    majority = set()
+    if n == 2:
+        if cows[0] == cows[1]:
+            return [cows[0]]
+        else:
+            return [-1]
+    for i in range(n-2):
+        if cows[i] == cows[i + 1]:
+            majority.add(cows[i])
+        elif cows[i] == cows[i + 2]:
+            majority.add(cows[i])
+        elif cows[i + 1] == cows[i + 2]:
+            majority.add(cows[i + 1])
+
+    if len(majority) == 0:
+        return [-1]
+
+    return sorted(list(majority))
+
+#read input
+t = int(input().strip())
+
+for i in range(t):
+    n = int(input().strip())
+    cows = [int(i) for i in input().strip().split()]
+    sol = (solve(n,cows))
+    print(" ".join([str(e) for e in sol]))
+
